e0ab6273bf4f26a5cef04b821ba5933a23006854
hanker <jinhui0106@126.com>
2017-06-28 16:03:58 +0800

    添加了注释

--- app/src/main/java/cn/hanker/com/myflow/activity/Test.java
+++ app/src/main/java/cn/hanker/com/myflow/activity/Test.java
@@ -2,9 +2,11 @@ package cn.hanker.com.myflow.activity;
 
 /**
  * @auther jh
- * @des ${TODO}
+ * @des 添加注释
  * Created by J.H on 2017/6/28.
  */
 
-public class Test {
+public interface Test {
+
+
 }
